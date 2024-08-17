c58a80f2bb32f76abf580f84792de567456f53ce
Ashadul Islam <ashadulmridhaprog@gmail.com>
2024-08-17 21:42:42 +0600

    feat: Double Link List prepend add

--- Link List/doubleLinkedList.py
+++ Link List/doubleLinkedList.py
@@ -10,7 +10,7 @@ class DoubleLinkList:
         new_node = Node(7)
         self.head = new_node
         self.tail = new_node
-        self.lenght = 1
+        self.length = 1
 
     def printList(self):
         temp=self.head
@@ -28,11 +28,47 @@ class DoubleLinkList:
             self.tail.next = new_node
             new_node.prev = self.tail
             self.tail = new_node
-        self.lenght +=1
+        self.length +=1
         return True
+
+    def pop(self):
+        if self.length == 0:
+            return None
+        
+        temp = self.tail
+        if self.length == 1:
+            self.head = None
+            self.prev = None
+        else:
+            self.tail = temp.prev
+            self.tail.next = None
+            temp.prev = None
+        self.length -=1
+        return temp
+
+    def prepend(self, value):
+        new_node = Node(value)
+
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            temp = self.head
+
+            new_node.prev = None
+            new_node.next = temp
+            self.head = new_node
+        
+        self.length +=1
+        return True
+
+
 
 
 myLinkedList = DoubleLinkList(7)
 myLinkedList.append(2)
+myLinkedList.append(3)
+myLinkedList.prepend(4)
+myLinkedList.pop()
 
 myLinkedList.printList()
