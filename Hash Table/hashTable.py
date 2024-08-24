c449addbaf79a7bb23f607d5b6371ff611f54841
Ashadul Islam <ashadulmridhaprog@gmail.com>
2024-08-25 01:16:31 +0600

    feat: hash table implement and get and set

---
+++ Hash Table/hashTable.py
@@ -0,0 +1,45 @@
+class HashTable:
+    def __init__(self, size=7):
+        self.data_map = [None] * size
+
+    def __hash(self, key):
+        my_hash = 0
+        for letter in key:
+            my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
+        return my_hash  
+
+    def set_item(self, key, value):
+        index = self.__hash(key)
+
+        if self.data_map[index] == None:
+            self.data_map[index] = []
+        self.data_map[index].append([key, value])
+
+    def get_item(self, key):
+        index = self.__hash(key)
+
+        if self.data_map[index] is not None:
+            for i in range(len(self.data_map[index])):
+                if self.data_map[index][i][0] == key:
+                    return self.data_map[index][i][1]
+
+        return None
+
+    def print_table(self):
+        for i, val in enumerate(self.data_map): 
+            print(i, ": ", val)
+
+    def returnDataMap(self):
+        return self.data_map
+
+        
+my_hash_table = HashTable()
+
+my_hash_table.set_item('boat', 400)
+my_hash_table.set_item('water', 500)
+my_hash_table.set_item('bus', 600)
+
+# print(my_hash_table.get_item('boat'))
+print(my_hash_table.returnDataMap())
+
+my_hash_table.print_table()
