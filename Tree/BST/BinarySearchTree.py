4b02d5a5f73e9cde211cdb361488abc8d54c79ee
Ashadul Islam <ashadulmridhaprog@gmail.com>
2024-08-31 19:55:47 +0600

    feat: breathfast search

--- Tree/BST/BinarySearchTree.py
+++ Tree/BST/BinarySearchTree.py
@@ -47,15 +47,37 @@ class BinarySearchTree:
                 return True
         return False
 
+    def breadFirstSearch(self):
+        queue = []
+        results = []
+
+        temp = self.root
+        queue.append(temp)
+
+        while len(queue) > 0:
+            element = queue.pop(0)
+            results.append(element.value)
+
+            if element.left is not None:
+                queue.append(element.left)
+            
+            if element.right is not None:
+                queue.append(element.right)
+        return results
 
 
 binarySearchTree = BinarySearchTree()
-binarySearchTree.insert(2)
-binarySearchTree.insert(3)
-binarySearchTree.insert(1)
+binarySearchTree.insert(47)
+binarySearchTree.insert(21)
+binarySearchTree.insert(76)
+binarySearchTree.insert(18)
+binarySearchTree.insert(27)
+binarySearchTree.insert(52)
+binarySearchTree.insert(82)
 
 # print(binarySearchTree.root.value)
 # print(binarySearchTree.root.left.value)
 # print(binarySearchTree.root.right.value)
 
-print(binarySearchTree.contain(1))
+# print(binarySearchTree.contain(1))
+print(binarySearchTree.breadFirstSearch())
